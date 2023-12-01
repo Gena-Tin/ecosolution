8bf51a2982df1c7ac77338f80541979d98469405
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2023-12-02 00:55:08 +0200

    created Electricity component and counter

--- src/components/Electricity/Electricity.js
+++ src/components/Electricity/Electricity.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import css from "./Electricity.module.css";
+import SectionTitle from "../CommonComponents/SectionTitle/SectionTitle";
 
 function Electricity() {
+  const [counter, setCounter] = useState(
+    Math.floor((Date.now() - new Date("1980-01-01").getTime()) / 1000)
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const formattedCounter = counter
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
   return (
-    <div id="service" style={{ height: "550px" }}>
-      Electricity
-    </div>
+    <section id="service" className={css.electricitySection}>
+      <div className="container">
+        <div className={css.titleContainer}>
+          <SectionTitle
+            title="Electricity we produced for all time"
+            alignCenter="true"
+          />
+        </div>
+        <div className={css.content}>
+          <div className={css.counterBlock}>
+            <span className={css.counter}>{formattedCounter}</span>
+            <span className={css.counterUnits}>kWh</span>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
