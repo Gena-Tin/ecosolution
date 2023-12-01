b4f49fd423358c350dfb59cb94acfa9266596127
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2023-12-01 22:15:48 +0200

    created FAQ component

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -2,8 +2,16 @@ import React from "react";
 import Accordion from "./Accordion/Accordion";
 import css from "./Faq.module.css";
 import SectionTitle from "../CommonComponents/SectionTitle/SectionTitle";
+import { ReactComponent as IcoContactUs } from "./images/ellipse.svg";
 
 function Faq() {
+  const scrollToContact = () => {
+    const contactSection = document.getElementById("contact-us");
+    if (contactSection) {
+      contactSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <section id="faq" className={css.faqSection}>
       <div className="container">
@@ -12,6 +20,12 @@ function Faq() {
         </div>
         <div className={css.accordionContainer}>
           <Accordion />
+        </div>
+        <div className={css.contactUsContainer}>
+          <p>Didn't find the answer to your question?</p>
+          <button className={css.buttonGContactUs} onClick={scrollToContact}>
+            Contact Us <IcoContactUs />
+          </button>
         </div>
       </div>
     </section>
