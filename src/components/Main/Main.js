070ad217b0837369590b3858cf7784ad8959d537
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2023-12-02 18:43:15 +0200

    added Main section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,45 @@
 import React from "react";
+import css from "./Main.module.css";
+import { ReactComponent as IcoArrowRight } from "./images/arrow-right.svg";
 
 function Main() {
+  const scrollToCases = () => {
+    const casesSection = document.getElementById("cases");
+    if (casesSection) {
+      casesSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
-    <div id="main" style={{ height: "550px" }}>
-      Main
-    </div>
+    <section id="main" className={css.mainSection}>
+      <div className="container">
+        <div className={css.content}>
+          <div className={css.titleContainer}>
+            <h1 className={css.title}>RENEWABLE ENERGY For any task</h1>
+          </div>
+          <div className={css.textContainer}>
+            <p className={css.text}>
+              Development and implementation of renewable non-polluting energy
+              sources, generating power generation using energy wind, sun,
+              water, biomass
+            </p>
+            <div className={css.buttonContainer}>
+              <button className={css.buttonStyle} onClick={scrollToCases}>
+                LearnMore
+                <div className={css.arrowCircle}>
+                  <IcoArrowRight />
+                </div>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className={css.contactsContainer}>
+          <p>79005, Ukraine, Lvivstreet. Shota Rustaveli, 7</p>
+          <p>office@ecosolution.com</p>
+          <p className={css.tradeMark}>ecosolution © 2023</p>
+        </div>
+        <div className={css.heroImg}></div>
+      </div>
+    </section>
   );
 }
 
